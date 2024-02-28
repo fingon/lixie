80e8a1ce8d5abc5be328296ccc329b847037d82e
Markus Stenberg <markus.stenberg@iki.fi>
2024-02-28 08:56:52 +0200

    main: Reorganize it to be more testable and clean

--- main.go
+++ main.go
@@ -8,12 +8,14 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
 	"slices"
 	"strconv"
 
@@ -55,10 +57,6 @@ func (self *Database) nextLogRuleId() int {
 	return id
 }
 
-type LogRuleEditHandler struct {
-	Database *Database
-}
-
 func debugRequest(r *http.Request) {
 	// Loop over header names
 	for name, values := range r.Header {
@@ -75,82 +73,98 @@ func debugRequest(r *http.Request) {
 
 }
 
-func (self LogRuleEditHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	rule, err := NewLogRuleFromForm(r)
-	if err != nil {
-		// TODO log error?
-		return
-	}
-	if r.FormValue(actionSave) != "" {
-		// Look for existing rule first
-		for _, v := range self.Database.LogRules {
-			if v.Id == rule.Id {
-				fmt.Printf("Rewrote matchers of rule %d\n", v.Id)
-				v.Matchers = rule.Matchers
-				http.Redirect(w, r, "/rule/", http.StatusSeeOther)
+type LogRuleEditHandler struct {
+}
+
+func logRuleEditHandler(db *Database) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		rule, err := NewLogRuleFromForm(r)
+		if err != nil {
+			// TODO log error?
+			return
+		}
+		if r.FormValue(actionSave) != "" {
+			// Look for existing rule first
+			for _, v := range db.LogRules {
+				if v.Id == rule.Id {
+					fmt.Printf("Rewrote matchers of rule %d\n", v.Id)
+					v.Matchers = rule.Matchers
+					http.Redirect(w, r, "/rule/", http.StatusSeeOther)
+					return
+				}
+			}
+
+			// Not found. Add new one.
+			fmt.Printf("Adding new rule\n")
+			db.Add(*rule)
+			http.Redirect(w, r, "/rule/", http.StatusSeeOther)
+			return
+		}
+		LogRuleEdit(*rule).Render(r.Context(), w)
+	})
+}
+
+func logRuleDeleteSpecificHandler(db *Database) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rid_string := r.PathValue("id")
+		rid, err := strconv.Atoi(rid_string)
+		if err != nil {
+			// TODO handle error
+			return
+		}
+		if db.Delete(rid) {
+			http.Redirect(w, r, "/rule/", http.StatusSeeOther)
+			return
+		}
+		http.NotFound(w, r)
+	})
+}
+
+func logRuleEditSpecificHandler(db *Database) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rid_string := r.PathValue("id")
+		rid, err := strconv.Atoi(rid_string)
+		if err != nil {
+			// TODO handle error
+			return
+		}
+		for _, v := range db.LogRules {
+			if v.Id == rid {
+				LogRuleEdit(*v).Render(r.Context(), w)
 				return
 			}
 		}
-
-		// Not found. Add new one.
-		fmt.Printf("Adding new rule\n")
-		self.Database.Add(*rule)
-		http.Redirect(w, r, "/rule/", http.StatusSeeOther)
-		return
-	}
-	LogRuleEdit(*rule).Render(r.Context(), w)
+		http.NotFound(w, r)
+	})
 }
 
-type LogRuleDeleteSpecificHandler struct {
-	Database *Database
-}
+func logRuleListHandler(db *Database) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		LogRuleList(db.LogRules).Render(r.Context(), w)
 
-func (self LogRuleDeleteSpecificHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	rid_string := r.PathValue("id")
-	rid, err := strconv.Atoi(rid_string)
-	if err != nil {
-		// TODO handle error
-		return
-	}
-	if self.Database.Delete(rid) {
-		http.Redirect(w, r, "/rule/", http.StatusSeeOther)
-		return
-	}
-	http.NotFound(w, r)
-}
-
-type LogRuleEditSpecificHandler struct {
-	Database *Database
-}
-
-func (self LogRuleEditSpecificHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	rid_string := r.PathValue("id")
-	rid, err := strconv.Atoi(rid_string)
-	if err != nil {
-		// TODO handle error
-		return
-	}
-	for _, v := range self.Database.LogRules {
-		if v.Id == rid {
-			LogRuleEdit(*v).Render(r.Context(), w)
-			return
-		}
-	}
-	http.NotFound(w, r)
-}
-
-type LogRuleListHandler struct {
-	Database *Database
-}
-
-func (self LogRuleListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	LogRuleList(self.Database.LogRules).Render(r.Context(), w)
+	})
 }
 
 //go:embed all:static
 var embedContent embed.FS
 
-func main() {
+// These might be also useful at some point
+//
+//	getenv func(string) string,
+//	stdin  io.Reader,
+//	stdout, stderr io.Writer,
+func run(
+	ctx context.Context,
+	args []string) error {
+
+	// This would be relevant only if we handled our own context.
+	// However, http.ListenAndServe catches os.Interrupt so this
+	// is not necessary:
+	//
+	//ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	//defer cancel()
+
 	// Sample content
 	rule := LogRule{Id: 1,
 		Matchers: []LogFieldMatcher{
@@ -160,6 +174,15 @@ func main() {
 	rules := []*LogRule{&rule}
 	db := Database{LogRules: rules}
 
+	// CLI
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	address := flags.String("address", "127.0.0.1", "Address to listen at")
+	port := flags.Int("port", 8080, "Port number to listen at")
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	// Static content
 	static_fs, err := fs.Sub(embedContent, "static")
 	if err != nil {
 		log.Panic(err)
@@ -170,19 +193,28 @@ func main() {
 	// Other options: StatusMovedPermanently, StatusFound
 	//	http.Redirect(w, r, "/rule/edit", http.StatusSeeOther)
 	//})
-	http.Handle("/", templ.Handler(MainPage()))
-	http.Handle("/rule/", LogRuleListHandler{&db})
-	http.Handle("/rule/edit", LogRuleEditHandler{&db})
-	http.Handle("/rule/{id}/delete", LogRuleDeleteSpecificHandler{&db})
-	http.Handle("/rule/{id}/edit", LogRuleEditSpecificHandler{&db})
+	http.HandleFunc("/", http.NotFound)
+	main_handler := templ.Handler(MainPage())
+	http.Handle("/{$}", main_handler)
+	http.Handle("/index.html", main_handler)
+	http.Handle("/rule/{$}", logRuleListHandler(&db))
+	http.Handle("/rule/edit", logRuleEditHandler(&db))
+	http.Handle("/rule/{id}/delete", logRuleDeleteSpecificHandler(&db))
+	http.Handle("/rule/{id}/edit", logRuleEditSpecificHandler(&db))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(static_fs))))
-
-	// CLI
-	address := flag.String("address", "127.0.0.1", "Address to listen at")
-	port := flag.Int("port", 8080, "Port number to listen at")
 
 	// Start the actual server
 	endpoint := fmt.Sprintf("%s:%d", *address, *port)
 	fmt.Printf("Listening on %s\n", endpoint)
 	http.ListenAndServe(endpoint, nil)
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	err := run(ctx, os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%w\n", err)
+		os.Exit(1)
+	}
 }
