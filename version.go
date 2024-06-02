01e71033f01cd4b6d51ee308c2810ef56eba71bf
Markus Stenberg <markus.stenberg@iki.fi>
2024-06-02 18:16:16 +0300

    Converted /version endpoint to dump json

--- version.go
+++ version.go
@@ -4,38 +4,45 @@
  * Copyright (c) 2024 Markus Stenberg
  *
  * Created:       Sun Jun  2 10:46:21 2024 mstenber
- * Last modified: Sun Jun  2 10:47:10 2024 mstenber
- * Edit time:     1 min
+ * Last modified: Sun Jun  2 18:16:15 2024 mstenber
+ * Edit time:     6 min
  *
  */
 
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"net/http"
 	"runtime/debug"
 )
 
+type VersionInfo struct {
+	BuildTimestamp string
+	BuildInfo      debug.BuildInfo
+}
+
 var BuildTimestamp = "not set"
 
 func versionHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		bi, ok := debug.ReadBuildInfo()
+		bi, _ := debug.ReadBuildInfo()
 		w.WriteHeader(http.StatusOK)
 		if r.FormValue("simple") != "" {
 			_, _ = io.WriteString(w, BuildTimestamp)
 			return
 		}
-		_, _ = io.WriteString(w, fmt.Sprintf("Build timestamp: %s\n", BuildTimestamp))
-		if !ok {
-			_, _ = io.WriteString(w, "No build info available\n")
 
+		vi := VersionInfo{BuildTimestamp: BuildTimestamp}
+		if bi != nil {
+			vi.BuildInfo = *bi
+		}
+		b, err := json.Marshal(vi)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
 			return
 		}
-		for _, setting := range bi.Settings {
-			_, _ = io.WriteString(w, fmt.Sprintf("%s=%s\n", setting.Key, setting.Value))
-		}
+		_, _ = w.Write(b)
 	})
 }
